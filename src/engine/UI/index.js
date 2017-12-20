c683bf91dc15b3c98aa7845eabe7ac84247ebeef
sonyahon <volkov030@gmail.com>
2017-12-20 20:01:18 +0300

    add commentary

--- src/engine/UI/index.js
+++ src/engine/UI/index.js
@@ -14,6 +14,13 @@ function GUI(root_element) {
   this.el = createElement('div', 'JJS-GUI');
   root_element.appendChild(this.el);
 
+  this.top_bar = null; // Top bar menu with some functions
+  this.left_panel = null; // Area wehere u can place (dragNDrop) some active windows
+  this.right_panel = null; // Area where u can place some more active windows
+  this.editor = null; // Editor of the game
+
+  // By default there is this functionality, but u can change it how u want with dnd or with the layout tool;
+
 }
 
 GUI.prototype.start = function() {
